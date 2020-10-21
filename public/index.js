0af770ae4be25cac90be63ee62ca3a863189bbd4
Aynat <aynatcm@gmail.com>
2020-10-21 02:50:34 -0600

    test final v2

--- public/index.js
+++ public/index.js
@@ -1,9 +1,21 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW registrado");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW registro fallo");
-        console.log(error);
-    })
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.register("sw.js").then(registration => {
+//         console.log("SW registrado");
+//         console.log(registration);
+//     }).catch(error => {
+//         console.log("SW registro fallo");
+//         console.log(error);
+//     })
+// }
+
+
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./service-worker.js')
+          .then((reg) => {
+            console.log('Service worker registered.', reg);
+          });
+    });
+  }
