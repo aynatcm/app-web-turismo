2fa4525f58c9d5c4c4d1755f92ea593a7cdbfee4
Aynat <aynatcm@gmail.com>
2020-10-21 02:07:20 -0600

    test de service worker v2

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', async e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./hoteles.html", "./Aventuras.html", "./lugares.html", "./Restaurantes.html", "./Boaco.html", "./Carazo.html", "Chinandega.html", "./Chontales.html", "./Esteli.html", "./Granada.html", "./Jinotega.html", "./Leon.html", "./Madriz.html", "./managua.html", "./Masaya.html", "./Matagalpa.html", "./NuevaSegovia.html", "./Raan.html", "./Raas.html", "./RioSanJuan.html", "./Rivas.html", "./css/activades.css",
+            return cache.addAll(["./", "./css/activades.css",
                 "./css/home.css", "./css/hoteles.css", "./css/lugares.css", "./css/restaurantes.css", "./css/Departamento.css", "./images/icons/icon-192x192.png"
             ]);
         })
